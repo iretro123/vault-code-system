699166bdfafccd685dd5079121ac3fb3302f56c9
user <user@Mac.lan>
2026-07-24 22:20:07 -0400

    Use customer role for iOS membership unlock

--- supabase/functions/activate-ios-membership/index.ts
+++ supabase/functions/activate-ios-membership/index.ts
@@ -12,7 +12,7 @@ const corsHeaders = {
     "authorization, x-client-info, apikey, content-type, x-supabase-client-platform, x-supabase-client-platform-version, x-supabase-client-runtime, x-supabase-client-runtime-version",
 };
 
-const FULL_ACCESS_ROLE = "vault_os_owner";
+const FULL_ACCESS_ROLE = "vault_access";
 const SHARED_GUEST_EMAIL = "guest@vaulttradingacademy.com";
 const DEFAULT_BUNDLE_ID = "com.vaulttradingacademy.vaultos";
 const DEFAULT_PRODUCT_ID = "com.vaulttradingacademy.vaultos.fullaccess.monthly99v2";
